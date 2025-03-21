784dcd0f12dbda4a9b140f178f85afe05bceacdc
Sachin bk <sachinbk.official@gmail.com>
2025-03-21 16:03:51 +0545

    updated

---
+++ day-5(basic javascript)/js/index.js
@@ -0,0 +1,16 @@
+// alert("you sure");
+// if (confirm("you sure")) {
+//     console.log("this is my first class")
+// }
+
+let  xyz=confirm("are you sure");
+console.log("this is my conformation", xyz);
+if (xyz==true) {
+    console.log("request accepted");
+    
+} else {
+    console.log("request rejected");
+}
+
+let yourPrompt = prompt("enter your name");
+console.log("hello", yourPrompt);
